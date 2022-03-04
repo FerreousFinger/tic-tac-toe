a487c64ad62100ad8452edb94c9db0dff69a5b02
FerreousFinger <32906407+FerreousFinger@users.noreply.github.com>
2022-03-04 23:13:25 +0100

    Remove constants from classes

--- main.py
+++ main.py
@@ -13,15 +13,16 @@ class Board:
     """Checker style board."""
 
     def __init__(self, size_x, size_y):
+        self.size_x = size_x
+        self.size_y = size_y
         self.size = int(size_x * size_y)
         self.grid = [[EMPTY] * size_x for _ in range(size_y)]
 
     def get_row(self, pos: int) -> int:
-        return len(self.grid) - (pos - 1)//BOARD_SIZE_Y - 1
+        return len(self.grid) - (pos - 1)//self.size_y - 1
 
-    @staticmethod
-    def get_column(pos: int) -> int:
-        return pos % BOARD_SIZE_X - 1
+    def get_column(self, pos: int) -> int:
+        return pos % self.size_x - 1
 
     def is_empty(self, pos: int) -> bool:
         return self.grid[self.get_row(pos)][self.get_column(pos)] == EMPTY
@@ -33,10 +34,13 @@ class Board:
 class ConsoleInterface:
     """Console user interface."""
 
-    @staticmethod
-    def print_board(board: Board):
-        rows = [f' {H_SEPERATOR} '.join(row) for row in board.grid]
-        h_line = ''.join([V_SEPERATOR * len(rows[0])])
+    def __init__(self, h_sep, v_sep):
+        self.h_sep = h_sep
+        self.v_sep = v_sep
+
+    def print_board(self, board: Board):
+        rows = [f' {self.h_sep} '.join(row) for row in board.grid]
+        h_line = ''.join([self.v_sep * len(rows[0])])
         board_str = f'\n{h_line}\n'.join(rows)
         print(board_str)
 
